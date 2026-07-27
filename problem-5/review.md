15716c62c73d814552992d78b0909b6003c16259
Robert Ling <lingrobert929@gmail.com>
2026-07-27 12:02:34 +1000

    problem 5

---
+++ problem-5/review.md
@@ -0,0 +1,3 @@
+Had to just brute force it today, wasn't away of any mathematical identies or strategies that would help me solve it.
+Had I really wanted to generalise, my strategy would to have been to write a helper function that would do a prime decomposition of every number
+to obtain the factors. Then I would create a hashmap to store all of the maximum number of times a prime factor appears 
---
+++ problem-5/solution.js
@@ -0,0 +1,23 @@
+const pf_2 = [2]
+const pf_3 = [3]
+const pf_4 = [2, 2]
+const pf_5 = [5]
+const pf_6 = [2, 3]
+const pf_7 = [7]
+const pf_8 = [2, 2, 2]
+const pf_9 = [3, 3]
+const pf_10 = [2, 5]
+const pf_11 = [11]
+const pf_12 = [2, 2, 3]
+const pf_13 = [13]
+const pf_14 = [2, 7]
+const pf_15 = [3, 7]
+const pf_16 = [2, 2, 2, 2]
+const pf_17 = [17]
+const pf_18 = [2, 3, 3]
+const pf_19 = [19]
+const pf_20 = [2, 2, 5]
+
+const res = 2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+
+console.log(res)
